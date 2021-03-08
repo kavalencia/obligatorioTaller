5d3ae685e2a35e12cc8990842750f664ee93d1c7
Agustin Verdera <agustin@kzsoftworks.com>
2021-03-08 14:24:13 -0300

    arreglo en insert comentario y refrescar comentarios

--- Templates/comentariosJuegoPaginados.tpl
+++ Templates/comentariosJuegoPaginados.tpl
@@ -22,5 +22,28 @@
     <div>
         <button id="anterior"{if ($pagina <= 0)} disabled {/if}>Anterior</button>
         <button id="siguiente"{if ($pagina >= $ultimaPagina)} disabled {/if}>Siguiente</button>
-    </div>    
+    </div>  
+    <div>
+        {if isset($usuarioLogueado)}
+            {if (!$tieneComentario)}
+                <h2>Tu comentario y puntaje nos interesa</h2>  
+                <textarea id="nuevoCom" rows="5" cols="40">Ingresa tu comentario aquí</textarea>
+                <p class="puntaje">
+                    <input id="radio1" type="radio" name="estrellas" value="5">
+                    <label for="radio1">★</label>
+                    <input id="radio2" type="radio" name="estrellas" value="4">
+                    <label for="radio2">★</label>
+                    <input id="radio3" type="radio" name="estrellas" value="3">
+                    <label for="radio3">★</label>
+                    <input id="radio4" type="radio" name="estrellas" value="2">
+                    <label for="radio4">★</label>
+                    <input id="radio5" type="radio" name="estrellas" value="1">
+                    <label for="radio5">★</label>
+                </p>
+                <input id="buttonComPun" type="button" value="Insertar">
+            {else}
+                <h3> Ya tienes un comentario y puntuacion para este juego </h3>
+            {/if}
+        {/if}
+    </div> 
 </div>
--- comentariosJuegoPaginados.php
+++ comentariosJuegoPaginados.php
@@ -5,13 +5,14 @@ require_once 'function.php';
 error_reporting(E_ERROR);
 
 $mySmarty = getSmarty();
-
+$tieneComentario = false;
 $pagina = 0;
+$texto = "";
+
+
 if (isset($_GET['pag'])) {
     $pagina = $_GET['pag'];
 }
-
-$texto = "";
 if (isset($_GET['texto'])) {
     $texto = $_GET['texto'];
 }
@@ -28,20 +29,39 @@ if (isset($_GET['borrar'])) {
     $borrar = $_GET['borrar'];
 }
 
-if($borrar == "1"){
-    borrarComentario($comId);
-    updatePuntuacionJuego($juegoId);
+if (isset($_GET['puntaje'])) {
+    $puntaje = $_GET['puntaje'];
+}
+
+if (isset($_GET['com'])) {
+    $com = $_GET['com'];
+}
+
+if (isset($_GET['inicio'])) {
+    $inicio = $_GET['inicio'];
 }
 
 session_start();
 $usuarioLogueado = NULL;
 if (isset($_SESSION['usuarioLogueado'])) {
     $usuarioLogueado = $_SESSION['usuarioLogueado'];
+    $tieneComentario = getTieneComentarioParaJuego($juegoId, $usuarioLogueado['id']);
 }
+
+if (!$borrar && $inicio == 0 && !$tieneComentario){   
+    insertComentarioJuego($com, $puntaje, $juegoId, $usuarioLogueado['id']);
+}
+
+if($borrar){
+    borrarComentario($comId);
+}
+updatePuntuacionJuego($juegoId);
+$tieneComentario = getTieneComentarioParaJuego($juegoId, $usuarioLogueado['id']);
 
 $comentarios = getComentariosDeJuego($pagina, $texto, $juegoId);
 $ultimaPagina = ultimaPaginaDeComentariosDelJuego($texto, $juegoId);
 
+$mySmarty->assign("tieneComentario", $tieneComentario);
 $mySmarty->assign("comentarios", $comentarios);
 $mySmarty->assign("pagina", $pagina);
 $mySmarty->assign("ultimaPagina", $ultimaPagina);
